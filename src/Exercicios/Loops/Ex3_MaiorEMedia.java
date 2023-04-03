7390012220461a5728006c27fd8c525336d7cbd0
freddyvidigal <freddy181208@hotmail.com>
2023-04-03 19:15:18 -0300

    media e soma

--- src/Exercicios/Loops/Ex3_MaiorEMedia.java
+++ src/Exercicios/Loops/Ex3_MaiorEMedia.java
@@ -1,5 +1,39 @@
 package Exercicios.Loops;
 
+import java.util.Scanner;
+
+/*
+ * faça um programa que leia 5 numeros
+ * e informe o maior numero
+ * e a media dos numero
+ */
 public class Ex3_MaiorEMedia {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        int soma = 0;
+
+        int count = 0;
+
+        do {
+            System.out.println("Digite um numero: ");
+            numero = input.nextInt();
+
+            soma += numero;
+
+            if (numero > maior) {
+                maior = numero;
+            }
+
+            count++;
+        } while (count <= 5);
+
+        System.out.println("O maior numero digitado foi: " + maior);
+        System.out.println("A media dos numeros digitados foi: " + soma / 5);
+
+    }
 
 }

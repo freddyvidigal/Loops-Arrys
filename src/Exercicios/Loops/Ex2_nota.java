b6675b1f71d4d10afaf4442bde77faf5425adbf7
freddyvidigal <freddy181208@hotmail.com>
2023-04-03 18:40:13 -0300

    nota

--- src/Exercicios/Loops/Ex2_nota.java
+++ src/Exercicios/Loops/Ex2_nota.java
@@ -1,12 +1,26 @@
 package Exercicios.Loops;
 
+import java.util.Scanner;
+
 /*
 faça um programa que peça uma nota, entre zero e dez.
 Mostre uma mnesagem caso o valor seja invalido e continue pedindo até que o usuario informe um valor valido
 */
-/**
- * Ex2_nota
- */
+
 public class Ex2_nota {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Digite uma nota entre 0 e 10: ");
+        nota = input.nextInt();
+
+        while (nota < 0 || nota > 10) {
+            System.out.println("Nota invalida, digite novamente: ");
+            nota = input.nextInt();
+        }
+    }
 
 }

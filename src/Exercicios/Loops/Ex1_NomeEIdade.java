2dd51135c9be178dbab18d5a902221fb39853530
freddyvidigal <freddy181208@hotmail.com>
2023-04-03 18:28:57 -0300

    first commit

---
+++ src/Exercicios/Loops/Ex1_NomeEIdade.java
@@ -0,0 +1,37 @@
+package Exercicios.Loops;
+/*
+faça um programa que leia conjunto de dois valores, o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(para inserir o valor 0 no campo nome)
+*/
+
+import java.util.Scanner;
+
+public class Ex1_NomeEIdade {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            String nome;
+            int idade;
+
+            /*
+             * System.out.println("Nome: ");
+             * nome = scan.nextLine();
+             * 
+             * System.out.println("Idade: ");
+             * idade = scan.nextInt();
+             */
+
+            while (true) {
+                System.out.println("Nome: ");
+                nome = scan.next();
+                if (nome.equals("0"))
+                    break;
+
+                System.out.println("Idade: ");
+                idade = scan.nextInt();
+            }
+
+            System.out.println("continua");
+        }
+    }
+
+}
---
+++ src/Exercicios/Loops/Ex2_nota.java
@@ -0,0 +1,12 @@
+package Exercicios.Loops;
+
+/*
+faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mnesagem caso o valor seja invalido e continue pedindo até que o usuario informe um valor valido
+*/
+/**
+ * Ex2_nota
+ */
+public class Ex2_nota {
+
+}
